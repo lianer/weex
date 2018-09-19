0c60eb77605c8a672e3134e10b8894d0af6faece
Lou Yunlong <lianer@live.cn>
2018-09-19 11:41:17 +0800

    publish

---
+++ app/service/upload.js
@@ -0,0 +1,10 @@
+const Service = require('egg').Service
+
+class UploadService extends Service {
+  async upload () {
+    return {
+      code: 0,
+      message: ''
+    }
+  }
+}
---
+++ config/plugin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// had enabled by egg
+// exports.static = true;
+
+exports.pug = {
+  enable: true,
+  package: 'egg-view-pug'
+}
